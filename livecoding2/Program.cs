d872644a3dcc2ba251624ff6465d1e0ec97db6d6
Roman Tranquil <roma7d@gmail.com>
2019-11-19 18:42:18 -0500

    initial

---
+++ livecoding2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace livecoding2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
