64b9f15d5c15eeaf9edfd35210140e9e20c0edd4
Roman Tranquil <roma7d@gmail.com>
2019-11-19 19:11:11 -0500

    done

--- livecoding2/Program.cs
+++ livecoding2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace livecoding2
 {
@@ -6,7 +7,36 @@ namespace livecoding2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string s = Console.ReadLine();
+
+            Console.WriteLine(longRepeat(s));
+
         }
+
+
+        static int longRepeat(string s)
+        {
+            int longestSubstring = 1;
+            int temp = 1;
+
+            for(int i = 0; i < s.Length-1; i++)
+            {
+                if (s[i] == s[i+1])
+                {
+                    temp += 1;
+                }
+                else
+                {
+                    if(temp > longestSubstring)
+                        longestSubstring = temp;
+                    temp = 1;
+                }
+            }
+
+            return longestSubstring;
+
+        }
+
     }
 }
